904b8828dd04e4a96eb6be04b1230829f56e5d2f
yeajeong kang <bananana0118@gmail.com>
2022-12-14 21:40:53 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from "react"
+import useDebounceFunc from "./utils/useDebounceFunc";
+import { debounce } from 'lodash';
 import './App.css';
 
+
 function App() {
+  const [idx, setIdx] = useState(0);
+  const sections = 3
+  const DELAY = 3000
+
+
+
+  const wheelHandler = (e) => {
+
+
+    if (e.deltaY > 0) {     //scroll down
+      if (idx !== sections) {
+        setIdx(idx + 1)
+      }
+    } else if (e.deltaY < 0) { //scroll up
+      if (idx !== 0) {
+        setIdx(idx - 1)
+      }
+    }
+    console.log(idx)
+
+  }
+
+
+  const mouseScrollHandler = () => {
+    console.log(idx)
+    window.scrollTo({ top: window.innerHeight * idx, behavior: "smooth" });
+
+  }
+
+  useEffect(() => {
+    window.addEventListener("wheel", debounce(wheelHandler, 300))
+  })
+
+
+
+  // useDebounceFunc({ func: () => wheelScrollHandler(idx, setIdx, sections), delay: DELAY, deps: [] })
+  console.log(idx)
+  useEffect(() => {
+    console.log(idx)
+
+    mouseScrollHandler()
+
+
+  }, [idx])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ backgroundColor: "yellow", width: "100%", height: "100vh" }}></div>
+      <div style={{ backgroundColor: "red", width: "100%", height: "100vh" }}></div>
+      <div style={{ backgroundColor: "purple", width: "100%", height: "100vh" }}></div>
+
+    </div >
   );
 }
 
---
+++ src/utils/useDebounceFunc.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+const useDebounceFunc = (delay, func, deps) => {
+
+
+
+    console.log(delay)
+
+}
+
+export default useDebounceFunc
